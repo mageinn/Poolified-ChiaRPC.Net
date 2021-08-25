e4c21a446efc2e0b799b0d0e2435b35190d4191e
Playwo <eliaswolf2001@t-online.de>
2021-08-25 13:15:07 +0200

    Fix Coin#Name

--- ChiaRPC.Net/Models/Node/Coin/Coin.cs
+++ ChiaRPC.Net/Models/Node/Coin/Coin.cs
@@ -1,6 +1,7 @@
 ﻿using ChiaRPC.Parsers;
 using ChiaRPC.Utils;
 using System;
+using System.Linq;
 using System.Text.Json.Serialization;
 
 namespace ChiaRPC.Models
@@ -23,11 +24,30 @@ namespace ChiaRPC.Models
         }
 
         public HexBytes Name()
-            => (ParentCoinInfo + PuzzleHash + BitConverter.GetBytes(Amount)).Sha256();
-
+            => Serialize().Sha256();
         public HexBytes Serialize()
             => ParentCoinInfo +
-                PuzzleHash +
-                StreamableUtils.Serialize(Amount);
+               PuzzleHash +
+               IntToBytes(Amount);
+
+        //https://github.com/Chia-Network/clvm/blob/main/clvm/casts.py#L8
+        private static HexBytes IntToBytes(ulong v)
+        {
+            if (v == 0)
+            {
+                return HexBytes.Empty;
+            }
+
+            var b = BitConverter.IsLittleEndian
+            ? BitConverter.GetBytes(v).Reverse()
+            : BitConverter.GetBytes(v);
+
+            while (b.Count() > 1 && b.First() == ((b.ElementAt(1) & 0x80) > 0 ? 0xFF : 0))
+            {
+                b = b.Skip(1);
+            }
+
+            return HexBytes.FromBytes(b.ToArray());
+        }
     }
 }
